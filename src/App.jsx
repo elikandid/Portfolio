be8951e289bfec88460454aad337328e81c46dcf
elikandid <edogbey64@gmail.com>
2024-08-16 07:54:51 +0000

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 
 const App = () => {
